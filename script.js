bf1e30f0384e0dbbcca8d3a56e28e915ffad0bd7
Theo Smith <80037733+TheodorusSmith@users.noreply.github.com>
2021-05-20 13:29:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Hallo Winc Academy studenten")
+// Dit is een grote som
+/*
+let a = 1230941;
+let b = 1823794;
+let c = a * b;
+console.log(c);
+// f = d / e;
+let d = 637263;
+let e = 54;
+let f = d / e;
+console.log(f);
+*/
